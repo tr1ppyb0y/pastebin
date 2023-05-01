f06254469208dc5f47ebfba45cfdde37063c60a6
unknown <asatishobhit9@gmail.com>
2023-05-01 14:08:35 +0530

    Refactor urls.py to register urls using DefaultRouter instead of explicit binding.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,40 +1,15 @@
-from django.urls import path
-from rest_framework.renderers import StaticHTMLRenderer
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from snippets.views import SnippetViewSet, UserViewSet, api_root
+from snippets import views
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename="snippet")
+router.register(r'user', views.UserViewSet, basename="user")
 
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
 
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_class=[StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', api_root),
-    path('user/', user_list, name='user-list'),
-    path('user/<int:pk>', user_detail, name='user-detail'),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight')
+    path('', include(router.urls))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,22 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
-
-# Making two roots.
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
+# The api_root is being deleted because,
+# URLs which are registered by DefaultRouter,
+# will be displayed at toor URL. ;)
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
